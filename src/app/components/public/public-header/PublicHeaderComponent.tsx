4de87fd7398dafa6e50ee33c43f7ef3d98922b8d
RishabhRajSh <70883764+RishabhRajSh@users.noreply.github.com>
2023-12-19 19:17:05 +0530

    Banner Header Imporved

--- src/app/components/public/public-header/PublicHeaderComponent.tsx
+++ src/app/components/public/public-header/PublicHeaderComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import Style from "./PublicHeaderComponent.module.scss"
-import { Link } from "react-router-dom"
 
 const PublicHeaderComponent = () => {
     const [isScrolled, setIsScrolled] = useState(false)
@@ -11,15 +10,10 @@ const PublicHeaderComponent = () => {
         return () => window.removeEventListener("scroll", handleScroll)
     }, [])
 
-
-
     return <React.Fragment>
         <div className={`${Style['container']} ${isScrolled ? Style.scrolled : ""}`}>
-        <span>Rishabh Raj Sharma</span> 
-        {/* replace this to Link to = "linked in" */}
-        <Link to ='/'>About Me</Link>
-        <Link to ='/'>Projects</Link>
-        <Link to ='/'>Contact</Link>
+            <p>Rishabh Raj Sharma</p>
+            <button>Let's get connected</button>
         </div>
     </React.Fragment>
 }
--- src/app/components/public/section-banner-about-me/SectionBannerAboutMeComponent.tsx
+++ src/app/components/public/section-banner-about-me/SectionBannerAboutMeComponent.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Style from "./SectionBannerAboutMeComponent.module.scss";
+import PaperClipIcon from "../../../../assets/icon/paperclip_bold.svg";
+import FigmaIcon from "../../../../assets/icon/IconFigmaStrokeWhite.svg";
 
 const SectionBannerAboutMe = () => {
     return <React.Fragment>
@@ -7,10 +9,12 @@ const SectionBannerAboutMe = () => {
             <div className={Style["content"]}>
                 <div className={Style["text-wrapper"]}>
                     <h4>Ui/Ux Developer</h4>
-                    <h1>Hello, my name is Rishabh Raj Sharma</h1>
+                    <h1>Hi, my name is Rishabh Raj Sharma</h1>
                     <p> My professional journey revolves around crafting engaging and user-centric digital experiences. I bring creativity and innovation to the forefront, ensuring that every design element serves both aesthetic and functional purposes.</p>
-                    <button>Projects</button>
-                    <button>Linked In</button>
+                    <div className={Style['btn-wrapper']}>
+                        <button><img src={PaperClipIcon} /> Download CV</button>
+                        <button><img src={FigmaIcon} />View Figma</button>
+                    </div>
                 </div>
             </div>
         </div>
