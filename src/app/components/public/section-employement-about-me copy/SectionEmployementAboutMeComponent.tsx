881988579f7c8fa03c5223e79d507c1524c95b9c
RIshabh Raj Sharma <70883764+RishabhRajSh@users.noreply.github.com>
2023-12-20 06:24:55 +0530

    Update SectionEmployement with gallery

--- src/app/components/public/section-employement-about-me copy/SectionEmployementAboutMeComponent.tsx
+++ src/app/components/public/section-employement-about-me copy/SectionEmployementAboutMeComponent.tsx
@@ -5,8 +5,8 @@ import LogoAccuratics from "../../../../assets/icon/LogoAccuraticsSoftwares.svg"
 const SectionEmployement = () => {
     return <React.Fragment>
         <div className={Style['container']}>
-            <h2>Current Employement</h2>
             <div className={Style['page-wrapper']}>
+                <h2>Current Employement</h2>
                 <img src={LogoAccuratics} alt="Accuratics Softwares"></img>
                 <h3>Accuratics Softwares Private Limited</h3>
                 <p>November 2022 to Present</p>
@@ -18,6 +18,14 @@ const SectionEmployement = () => {
                     <li>Collaborated with developers, product managers, and stakeholders.</li>
                     <li>Used version control systems for Pull Requests, Merge Requests and Reviewing.</li>
                 </ul>
+                <div className{Style['gallery-row'}>
+                    <img />
+                    <img />
+                    <img />
+                    <img />
+                    <img />
+                    <img />
+                </div>
             </div>
         </div>
 
